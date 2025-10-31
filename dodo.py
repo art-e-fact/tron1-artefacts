0c7b5581fd11205a7f6bd68c924d94d71b222703
shamikarim <ahresgg@gmail.com>
2025-10-31 19:09:11 +0900

    Fixed the possible error with no stamps or src directory in doit.

--- dodo.py
+++ dodo.py
@@ -228,6 +228,7 @@ def task_download():
                     f"git clone {repo.link} {repo.src_dir} -b {repo.branch} || true"
                 )
             ],
+            "task_dep": ["workspace"],
             "uptodate": [os.path.exists(repo.src_dir)],
             "verbosity": 2,
         }
@@ -371,7 +372,7 @@ def task_rosdep():
             f"bash -lc \"echo 'stamp: {time()}' > {stamps}/.doit_rosdep.stamp\"",
         ],
         "uptodate": [check],
-        "task_dep": ["rosdep:update"]
+        "task_dep": ["rosdep:update", "workspace"]
         + [f"rosdep:{apt_pkg}" for apt_pkg in missing_rosdep],
         "targets": [f"{stamps}/.doit_rosdep.stamp"],
         "verbosity": 2,
