540807b91c727245506508f1330613404e2a8500
Tomo <68368403+tomonorman@users.noreply.github.com>
2025-11-06 16:15:56 +0900

    remove bash scripts as folks may use different shells / venvs (#10)

--- TEST.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-source $SCRIPT_DIR/SOURCE.bash
-set -e -o pipefail
-
-python -m pytest ./test/test_move.py \
-    -v \
-    -x \
